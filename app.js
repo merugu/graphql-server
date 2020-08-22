4f55a60ec6712ac835d39295bf1fb9eee9789cbd
merugu <merugu.amithesh@gmail.com>
2020-08-22 17:27:01 -0400

    enabled cors to publish the end point to client

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schemas/schema')
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
+
+//allow cross-origin request
+
+app.use(cors())
 
 //The below connection url you can get from the free account created online here
 //https://mlab.com/
-mongoose.connect("")
+mongoose.connect("pass your mongodb connection url here")
 mongoose.connection.once('open', () => {
     console.log('DB connection established');
 })
